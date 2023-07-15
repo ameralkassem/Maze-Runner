93354e82fd38ad1791e39ef1ff1e5a2feeecfd26
Mohammad Awada <awada@Mohammads-MacBook-Air.local>
2023-07-15 16:18:13 +0300

    creating map and player using create function

--- level2.js
+++ level2.js
@@ -1,4 +1,27 @@
 let gameScene = new Phaser.Scene("Game");
+gameScene.preload = function() {
+    this.load.image("wall-image","assets/wall-images.png");
+    this.load.image("player","assets/player-front.png");
+    this.load.spritesheet("gamepieces","assets/player-pieces.png", {
+        frameWidth : 60,
+        frameHeight : 65
+    })
+    this.load.tilemapTiledJSON("wallmap","game.json");
+};
+gameScene.create = function() {
+    const map = this.make.tilemap({key : "wallmap"});
+    const tileset = map.addTilesetImage("map","wall-image");
+    const floor_layer = map.createLayer("floor",tileset,0,0);
+    const grass_layer = map.createLayer("grass",tileset,0,0);
+    floor_layer.setCollisionByExclusion(-1,true);
+
+    this.player = this.physics.add.sprite(0,20,"player");
+    this.player.setBounce(0,1);
+    this.player.setCollideWorldBounds(true);
+    this.physics.add.collider(this.player,floor_layer);
+
+
+}
 const config = {
     type : Phaser.AUTO,
     width : 1280,
