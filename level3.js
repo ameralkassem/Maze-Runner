80f70acb2800fe99e1a2637c652aa305ae3516f0
MhdTarhini <mohamedtarhini95@gmail.com>
2023-07-16 02:26:59 +0300

    add enemy

--- level3.js
+++ level3.js
@@ -3,6 +3,7 @@ let gameScene = new Phaser.Scene("Game");
 gameScene.preload = function () {
   this.load.image("myTileset-image", "assets/wall-images.png");
   this.load.image("player", "assets/player-front.png");
+  this.load.image("enemy", "assets/enemy.png");
   this.load.spritesheet("gamePiece", "assets/player-pieces.png", {
     frameWidth: 60,
     frameHeight: 65,
@@ -19,8 +20,8 @@ gameScene.create = function () {
   const map = this.make.tilemap({ key: "Gamemap" });
   const tileset = map.addTilesetImage("map", "myTileset-image");
   const background_layer = map.createLayer("background", tileset, 0, 0);
-  const floor_layer = map.createLayer("floor", tileset, 0, 0);
   const grass_layer = map.createLayer("grass", tileset, 0, 0);
+  const floor_layer = map.createLayer("floor", tileset, 0, 0);
 
   floor_layer.setCollisionByExclusion(-1, true);
 
@@ -29,43 +30,15 @@ gameScene.create = function () {
   this.player.setCollideWorldBounds(true);
   this.physics.add.collider(this.player, floor_layer);
 
-  this.anims.create({
-    key: "left",
-    frames: this.anims.generateFrameNumbers("gamePiece", {
-      start: 7,
-      end: 4,
-    }),
-    frameRate: 10,
-    repeat: -1,
-  });
+  this.enemy = this.physics.add.sprite(400, 20, "enemy");
+  this.enemy.setBounce(0.1);
+  this.enemy.setCollideWorldBounds(true);
+  this.physics.add.collider(this.enemy, floor_layer);
+  this.enemy.setDisplaySize(80, 80);
 
-  this.anims.create({
-    key: "right",
-    frames: this.anims.generateFrameNumbers("gamePiece", {
-      start: 8,
-      end: 11,
-    }),
-    frameRate: 10,
-    repeat: -1,
-  });
-  this.anims.create({
-    key: "up",
-    frames: this.anims.generateFrameNumbers("gamePiece", {
-      start: 12,
-      end: 15,
-    }),
-    frameRate: 10,
-    repeat: -1,
-  });
-  this.anims.create({
-    key: "down",
-    frames: this.anims.generateFrameNumbers("gamePiece", {
-      start: 0,
-      end: 3,
-    }),
-    frameRate: 10,
-    repeat: -1,
-  });
+  this.enemy.body.setSize(80, 80);
+
+  this.physics.world.gravity.y = 500;
 };
 
 let config = {
